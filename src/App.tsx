0974e87882fce9e726f9b01021e4ceaa39bd5838
Kamila <nkf99@mail.ru>
2024-06-24 10:02:35 -0400

    fixed routers

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,29 @@ import Header from './components/Header'
 import About from './pages/About'
 import Home from './pages/Home'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home />,
+//   },
+//   {
+//     path: '/about',
+//     element: <About />,
+//   }
+// ])
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <RouterProvider router={router} />
-    </>
+      {/* <RouterProvider router={router} /> */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import './Header.css'
 import { headerSection } from '../store/index'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div className='header'>
-            <div className='header__section'>Home</div>
+            <Link to="/" className='header__section'>Home</Link>
             {headerSection.map((item, index) => (
-                <div className='header__section' key={index}>{item}</div>
+                <Link to="/about" className='header__section' key={index}>{item}</Link>
             ))}
         </div>
     )
