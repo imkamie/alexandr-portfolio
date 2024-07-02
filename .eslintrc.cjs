cd0e377a05f143ce6cbb3a3192ee92e4671a6d25
Kamila <nkf99@mail.ru>
2024-07-02 10:33:22 -0400

    fixed eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,32 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/react-in-jsx-scope': 'off', // Not necessary for React 17+
+    'react/prop-types': 'off', // We use TypeScript for prop types
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Disable requiring return types on functions
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Ignore unused variables with an underscore prefix
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the version of React to use
+    },
   },
 }
